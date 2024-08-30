721cba35761c00cab4202bda42ec478ad9f6bb87
Harsh Kumar Gupta <145157174+Harsh8831@users.noreply.github.com>
2024-08-30 21:24:33 +0530

    pages are linked

---
+++ src/landing_page/home/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hero() {
+        return (
+                <div className="container p-5 mb-5">
+                <div className="row text-center">
+                        <img src="images/homeHero.png" alt="hero" style={{width:"100%"}} className="mb-5 mx-auto"/>
+                        <h1 className="mt-5">Invest in everything.</h1>
+                        <p >Online platform for invest stocks, derivatives, mutual funds, and more</p>
+                        <button className="p-2 btn btn-primary fs-5 mb-5" style={{width: "20%", margin:"0 auto"}}>Get Started</button>
+        
+                </div>
+        </div> );
+}
+
+export default Hero;
